f8921110ea4b3706cbf7a2c23e33dccd168d858e
Peridot Lazuli <duckwhaleempire@gmail.com>
2020-12-01 03:51:07 -0700

    Add .gitignore

--- test.gd
+++ test.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _ready():
-	$RPGLabel.print_message("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 32.0)
+	$RPGLabel.print_message("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 32.0, ["res://addons/rpg_text_boxes/sine.wav"])
